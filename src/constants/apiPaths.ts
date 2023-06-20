622e05b46b1b8a5f62082fa3c15c4938ac008890
Valiantsin Anikin <valiantsin.anikin@bayer.com>
2023-06-20 22:20:43 +0200

    task-5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://geo7wpt106.execute-api.us-east-1.amazonaws.com/prod",
-  order: "https://geo7wpt106.execute-api.us-east-1.amazonaws.com/prod",
-  import: "https://z2qs2a8ndc.execute-api.us-east-1.amazonaws.com/prod",
-  bff: "https://geo7wpt106.execute-api.us-east-1.amazonaws.com/prod",
-  cart: "https://geo7wpt106.execute-api.us-east-1.amazonaws.com/prod",
+  product: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
+  order: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://jkwaeh28o3.execute-api.us-east-1.amazonaws.com/prod",
+  bff: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
+  cart: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
 };
 
 export default API_PATHS;
