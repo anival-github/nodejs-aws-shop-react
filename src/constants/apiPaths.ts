388a650d3af6bb86012bcdc2d2e66a3b58831206
anival-github <69170871+anival-github@users.noreply.github.com>
2023-06-28 23:24:34 +0200

    Task 5 (#2)
    * task-5
    
    * task-5
    
    * task-5
    
    ---------
    
    Co-authored-by: Valiantsin Anikin <valiantsin.anikin@bayer.com>

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
-  import: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://jkwaeh28o3.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://fst091zkd3.execute-api.us-east-1.amazonaws.com/prod",
 };
