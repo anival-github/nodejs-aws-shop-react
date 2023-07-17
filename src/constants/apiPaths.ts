3ade871792bf758fbddb4e0cb68e754711093819
Valiantsin Anikin <valiantsin.anikin@bayer.com>
2023-07-18 00:44:05 +0200

    task-9

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   import: "https://jkwaeh28o3.execute-api.us-east-1.amazonaws.com/prod",
   // import: "https://h63kd0ppvh.execute-api.us-east-1.amazonaws.com/prod",
   bff: "https://0x886qllp8.execute-api.us-east-1.amazonaws.com/prod",
-  cart: "http://development.eba-sjic3pmv.us-east-1.elasticbeanstalk.com/api",
+  cart: "https://development.eba-sjic3pmv.us-east-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
