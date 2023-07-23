e3531b46a2d8c06f06dc184f2c510d314bc0ecb3
Valiantsin Anikin <valiantsin.anikin@bayer.com>
2023-07-23 13:20:11 +0200

    task-10

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,8 +2,7 @@ const API_PATHS = {
   product: "https://0x886qllp8.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://0x886qllp8.execute-api.us-east-1.amazonaws.com/prod",
   import: "https://jkwaeh28o3.execute-api.us-east-1.amazonaws.com/prod",
-  // import: "https://h63kd0ppvh.execute-api.us-east-1.amazonaws.com/prod",
-  bff: "https://0x886qllp8.execute-api.us-east-1.amazonaws.com/prod",
+  bff: "http://anival-github-bff-api-dev.us-east-1.elasticbeanstalk.com/",
   cart: "https://development.eba-sjic3pmv.us-east-1.elasticbeanstalk.com/api",
 };
 
